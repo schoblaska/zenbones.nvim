454738cd050c561b66fdde66b5d10afe1418bf7c
Joey Schoblaska <joey.schoblaska@gmail.com>
2023-05-14 20:42:02 -0500

    override some specs

--- lua/neobones/init.lua
+++ lua/neobones/init.lua
@@ -3,6 +3,7 @@ local hsluv = lush.hsluv
 local generator = require "zenbones.specs"
 local bg = vim.o.background
 local p = require("neobones.palette")[bg]
+local theme = require "neobones"
 
 local specs = generator.generate(p, bg, generator.get_global_config("neobones", bg))
 
@@ -24,9 +25,17 @@ else
 		-- selene: allow(undefined_variable)
 		return {
 			FloatBorder { specs.FloatBorder, fg = hsluv "#203f57" }, -- --border-color
-			helpHyperTextJump { fg = hsluv "#8ec77e" }, -- --link-color
+			helpHyperTextJump { fg = hsluv "#658d57" }, -- --link-color
 			helpOption { helpHyperTextJump },
 			markdownUrl { helpHyperTextJump },
+
+			IndentBlanklineIndent1 { fg = p.bg.lighten(10) },
+			IndentBlanklineContextChar { fg = p.bg.lighten(25) },
+
+			LeapLabelPrimary { fg = p.bg, bg = p.blossom },
+			LeapBackdrop { fg = theme.Comment.fg },
+
+			LualineEmpty { fg = p.bg, bg = p.bg },
 		}
 		-- selene: deny(undefined_variable)
 		---@diagnostic enable: undefined-global
